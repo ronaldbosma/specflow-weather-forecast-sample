c738cf345b3cedefce317ede621f97f30e80ac08
Ronald Bosma <ronald.bosma@infosupport.com>
2022-02-11 17:46:03 +0100

    Change FetchData page to retrieve the forecast from the API

---
+++ src/UI/Apis/IWeatherForecastApi.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using WeatherForecastSample.Shared.Models;
+
+namespace WeatherForecastSample.UI.Apis
+{
+    [Headers("Content-Type: application/json")]
+    public interface IWeatherForecastApi
+    {
+        [Get("/weatherforecasts/comingweek")]
+        Task<IEnumerable<WeatherForecast>> GetWeatherForecastForComingWeekAsync();
+    }
+}
--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Refit;
 using WeatherForecastSample.UI;
+using WeatherForecastSample.UI.Apis;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddRefitClient<IWeatherForecastApi>()
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7288"))
+                .SetHandlerLifetime(TimeSpan.FromMinutes(2));
+
+
 
 await builder.Build().RunAsync();
