4b94b211c950c0d0fe854e58cdffe26263e4b789
Ronald Bosma <ronald.bosma@infosupport.com>
2022-02-12 15:00:07 +0100

    Display weather type images of coming week

---
+++ src/UI/Pages/IndexBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using WeatherForecastSample.Shared.Models;
+using WeatherForecastSample.UI.Apis;
+
+namespace WeatherForecastSample.UI.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        [Inject]
+        public IWeatherForecastApi Api { get; set; } = null!;
+
+        public IEnumerable<WeatherForecast> WeatherForecasts { get; set; } = new List<WeatherForecast>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            WeatherForecasts = await Api.GetWeatherForecastForComingWeekAsync();
+        }
+    }
+}
