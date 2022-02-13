1ca0e4e871c4e2b84772fa3b89c7ade4c9a2927d
Ronald Bosma <ronald.bosma@infosupport.com>
2022-02-13 12:19:27 +0100

    Extract Startup from Program

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,38 +1,17 @@
-using WeatherForecastSample.WebAPI;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddWeatherForecastDbContext();
-builder.Services.AddWeatherForecastDependencies();
-
-builder.Services.EnsureDatabaseExists();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace WeatherForecastSample.WebAPI
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-//DON'T USE IN PRODUCTION: this will allow the Blazor UI to call the Web API
-app.UseCors(cors => cors.AllowAnyMethod()
-                        .AllowAnyHeader()
-                        .SetIsOriginAllowed(origin => true)
-                        .AllowCredentials());
-
-app.Run();
-
---
+++ src/WebAPI/Startup.cs
@@ -0,0 +1,50 @@
+﻿namespace WeatherForecastSample.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddWeatherForecastDependencies();
+            services.AddWeatherForecastDbContext().EnsureDatabaseExists();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseRouting();
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            //DON'T USE IN PRODUCTION: this will allow the Blazor UI to call the Web API
+            app.UseCors(cors => cors.AllowAnyMethod()
+                                    .AllowAnyHeader()
+                                    .SetIsOriginAllowed(origin => true)
+                                    .AllowCredentials());
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
