ddc2f5eccdb743110dbb6d220b28bdd17654f5d0
Ronald Bosma <ronald.bosma@infosupport.com>
2022-02-14 19:17:37 +0100

    Start with 0 in WindDirection enum

--- src/Shared/Models/WindDirection.cs
+++ src/Shared/Models/WindDirection.cs
@@ -2,13 +2,13 @@
 {
     public enum WindDirection
     {
-        N = 1,
-        NE = 2,
-        E = 3,
-        SE = 4,
-        S = 5,
-        SW = 6,
-        W = 7,
-        NW = 8
+        N = 0,
+        NE = 1,
+        E = 2,
+        SE = 3,
+        S = 4,
+        SW = 5,
+        W = 6,
+        NW = 7
     }
 }
