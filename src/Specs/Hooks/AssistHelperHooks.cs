528598172a230e840c7b38f93e1e5d626a145872
Ronald Bosma <ronald.bosma@infosupport.com>
2022-02-16 20:50:55 +0100

    Value retrievers

---
+++ src/Specs/Hooks/AssistHelperHooks.cs
@@ -0,0 +1,15 @@
+﻿using TechTalk.SpecFlow.Assist;
+
+namespace WeatherForecastSample.Specs.Hooks
+{
+    [Binding]
+    internal class AssistHelperHooks
+    {
+        [BeforeTestRun]
+        public static void RegisterAssistHelpersBeforeTestRun()
+        {
+            Service.Instance.ValueRetrievers.Register(new DateOnlyValueRetriever());
+            Service.Instance.ValueComparers.Register(new DateOnlyValueComparer());
+        }
+    }
+}
