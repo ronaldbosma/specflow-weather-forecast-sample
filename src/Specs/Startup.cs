9b49d71f2c736709a74fc045b595c688dac34eab
Ronald Bosma <ronald.bosma@infosupport.com>
2022-02-16 21:44:36 +0100

    Comments

--- src/Specs/Startup.cs
+++ src/Specs/Startup.cs
@@ -13,13 +13,17 @@ namespace WeatherForecastSample.Specs
             var services = new ServiceCollection();
 
             services.AddWeatherForecastDependencies();
+
+            // Use in memory database
             services.AddDbContext<WeatherForecastDbContext>(
                 options => options.UseInMemoryDatabase("WeatherForecastSample.WeatherForecast"));
 
+            // Mock the authenticated user
             var authenticatedUser = new Mock<IAuthenticatedUser>();
             services.AddSingleton(authenticatedUser);
             services.AddSingleton(authenticatedUser.Object);
 
+            // Register Support classes
             services.AddTransient<DataContext>();
 
             return services;
